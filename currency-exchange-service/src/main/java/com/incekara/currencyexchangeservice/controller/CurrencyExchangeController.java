e53a227a992c1130234b46c939aa856963f5292c
unknown <enesincekara61@gmail.com>
2023-06-07 17:33:47 +0300

    initial commit

---
+++ currency-exchange-service/src/main/java/com/incekara/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -0,0 +1,39 @@
+package com.incekara.currencyexchangeservice.controller;
+
+import com.incekara.currencyexchangeservice.model.CurrencyExchange;
+import com.incekara.currencyexchangeservice.repository.CurrencyExchangeRepository;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+    private final Environment environment;
+
+    private final CurrencyExchangeRepository repository;
+    public CurrencyExchangeController(Environment environment, CurrencyExchangeRepository repository) {
+        this.environment = environment;
+        this.repository = repository;
+    }
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
+
+        CurrencyExchange currencyExchange=repository.findByFromAndTo(from,to);
+
+        if (currencyExchange==null){
+            throw new RuntimeException("Unable to find data for" + from + "to"+to);
+        }
+
+        String port= environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+
+        return currencyExchange;
+    }
+
+
+}
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10001,'USD','TL',21,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10002,'EUR','TL',22.30,'');
